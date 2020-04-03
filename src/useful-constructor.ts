65b2499bfadfaf2119fae1e90d0866ce2cb50b35
Max Beatty <mbeatty@squareup.com>
2020-04-03 13:04:59 -0700

    add example of false positive

---
+++ src/useful-constructor.ts
@@ -0,0 +1,18 @@
+declare class Animal {
+    constructor(age: number);
+    makeNoise: () => string;
+}
+
+export default class Calculator {
+  add(x: number, y: number): number {
+    return x + y;
+  }
+
+  sub(x: number, y: number): number {
+    return x - y;
+  }
+
+  surprise(pet: Animal) {
+      return pet.makeNoise()
+  }
+}
